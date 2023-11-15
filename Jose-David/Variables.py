b3d782217850ecaabc037fcd35bda561e1d00de7
DarckLuck <examplegit@gmai.com>
2023-11-15 13:51:19 -0500

    First task

---
+++ Jose-David/Variables.py
@@ -0,0 +1,40 @@
+#Constantes 
+
+PIEDRITA = 5
+NUMS = 8
+LEARNING = 'Books'
+SEE = 'With eyes'
+OPEN = 'The windows'
+
+#Numericos enteros
+
+Empanada = 4
+Año = 2023
+Mes = 11
+Día = 31
+Helado = 1.000
+
+# Numerico decimiales
+
+Math = 5.0
+Cience = 4.5
+Social = 3.8
+English = 4.0
+Spanish = 4.4
+
+# Strings
+
+String_1 = 'Hello world!'
+String_2 = 'Goood bye world!'
+String_3 = 'See you later'
+String_4 = 'See you tumorrow'
+String_5 = 'Hi'
+
+#Boolean
+
+Te_gusta_la_carne = True
+Estas_Mintiendo_= False
+
+print(PIEDRITA,NUMS,LEARNING,SEE,OPEN,Empanada,Año,Mes,Día,Helado,Math,Cience,Social,English,Spanish,String_1,String_2,String_3,String_4,String_5)
+
+
