a07507647f61fe23341db7ffe55b32a1dc9e3222
Weimar Tamayo <weimart.2010@gmail.com>
2023-12-16 09:19:22 -0500

    Se suben primeros archivos del curso de phyton

---
+++ weimar-tamayo/sesion_1.py
@@ -0,0 +1,12 @@
+papa = "Marcos"
+mama = "Celina"
+hermana = "Ana"
+sobrina = "Jimena"
+tio = "Oscar"
+
+telefono = 3024224041
+cedula = 1044502038
+edad = 36
+estatura = 1.69
+peso = 72
+print(papa, mama, hermana, sobrina, tio, telefono, cedula, edad, estatura, peso)
