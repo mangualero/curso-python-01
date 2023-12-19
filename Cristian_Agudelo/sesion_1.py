569de10a87ee329d76737469d6dae7687fb5407a
Cristian Agudelo <cristianmira39hotmail.com>
2023-12-19 13:26:49 -0500

    Cambios

---
+++ Cristian_Agudelo/sesion_1.py
@@ -0,0 +1,15 @@
+# Variables con datos de tipo cadena
+
+name = "Sergio"
+lastname = "Perez"
+municipality = "Santa Rosa de Osos"
+neighborhood = "Calle caldas"
+institute = "María Auxiliadora"
+
+# Variables con datos de tipo númerico
+
+id_number = 1056982365
+phone_number = 3137573948
+Age = 37
+number_of_children = 1
+number_of_votes= 350
